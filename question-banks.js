def3651ef67da8aad6aafa1f75a0ec214a929b7f
jp <james@pearce.com.au>
2026-07-31 20:38:19 +0930

    Add question banks for Tintinalli and Rosen

--- question-banks.js
+++ question-banks.js
@@ -1,1 +1,14 @@
-
+window.QUESTION_BANKS = [
+  {
+    id: "tintinalli",
+    label: "Tintinalli's Emergency Medicine Manual, 8th ed.",
+    src: "questions-tintinalli.js",
+    variable: "QUESTIONS_TINTINALLI"
+  },
+  {
+    id: "rosen",
+    label: "Rosen's Emergency Medicine: Concepts and Clinical Practice, 8th ed.",
+    src: "questions-rosen.js",
+    variable: "QUESTIONS_ROSEN"
+  }
+];
